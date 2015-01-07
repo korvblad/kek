85cb157df39057d6f80ac7f0f46e38dd3f791d7c
Tobias Hallgren <tobbiashallgren@gmail.com>
2015-01-07 09:33:47 +0100

    Först

--- Laboration1TH/Laboration1TH/Program.cs
+++ Laboration1TH/Laboration1TH/Program.cs
@@ -4,70 +4,130 @@ namespace Laboration1TH
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+            private const double femhundra = 500;
+            private const double etthundra = 100;
+            private const double tjugo = 20;
+            private const double fem = 5;
+            private const double en = 1;
             
-            double totalKostnad = 0;
-            int erhalletBelopp = 0;
-            double attBetala = 0;
-            double oresavrundning = 0;
-            double tillbaka = 0;
-            string inmatatTal;
 
-
-            while (true)
+            static void Main(string[] args)
             {
-                try
+
+                double totalKostnad = 0;
+                double erhalletBelopp = 0;
+                double attBetala = 0;
+                double oresavrundning = 0;
+                double tillbaka = 0;
+                string inmatatTal;
+
+                // Ger felmeddelande ifall något annat än double skrivs in
+                while (true)
                 {
-                    Console.Write("Ange summa att betala: ");
-                    inmatatTal = Console.ReadLine();
-                    totalKostnad = double.Parse(inmatatTal);
-                    break;
+                    try
+                    {
+                        Console.Write("Ange summa att betala: ");
+                        inmatatTal = Console.ReadLine();
+                        totalKostnad = double.Parse(inmatatTal);
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("FEL! Erhållet belopp felaktigt.");
+                        Console.ResetColor();
+                    }
                 }
-                catch (Exception)
+
+                //Ger felmeddelande ifall något annat än en int skrivs in
+                while (true)
                 {
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("FEL! Erhållet belopp felaktigt.");
-                    Console.ResetColor();
+                    try
+                    {
+                        Console.Write("Ange erhållet belopp: ");
+                        inmatatTal = Console.ReadLine();
+                        erhalletBelopp = int.Parse(inmatatTal);
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("FEL! Erhållet belopp felaktigt.");
+                        Console.ResetColor();
+                    }
                 }
-            }         
-            
-           
-            
-            Console.Write("Ange erhållet belopp: ");
-            inmatatTal = Console.ReadLine();
-            erhalletBelopp = int.Parse(inmatatTal);
-
-        
-
-
-            //Öresavrunding 
-            attBetala = (uint)Math.Round(totalKostnad);
-            oresavrundning = attBetala - totalKostnad;
-            
-            tillbaka = erhalletBelopp - attBetala;
 
 
 
-            Console.WriteLine("\nKVITTO\n-----------------------------");
-
-            Console.WriteLine("Totalt :   {0,20:c}\nÖresavrunding :{1,15:c}\nAtt betala :    {2,15:c}\nKontant       : {3,15:c}\nTillbaka :    {4,15:c}",
-                totalKostnad,
-                oresavrundning,
-                attBetala,
-                erhalletBelopp,
-                tillbaka);
-            Console.WriteLine("-----------------------------");
-
-            
-            
 
 
-            if (tillbaka > 500 )
-            {
-                Console.WriteLine("500-lapp");
+
+                //Öresavrunding 
+                attBetala = (uint)Math.Round(totalKostnad);
+                oresavrundning = attBetala - totalKostnad;
+
+                tillbaka = erhalletBelopp - attBetala;
+
+                string totalt = "Totalt";
+                string avrunding = "Öresavrundning";
+                string betala = "Att betala";
+                string erhallet = "Kontant";
+                string retur = "Tillbaka";
+
+                Console.WriteLine("\nKVITTO\n-----------------------------");
+
+                Console.WriteLine("{0,-17}:{1,8:F2}\n{2,-17}:{3,8:c}\n{4,-17}:{5,8}\n{6,-17}:{7,8}\n{8,-17}:{9,8}",
+                    totalt,
+                    totalKostnad,
+                    avrunding,
+                    oresavrundning,
+                    betala,
+                    attBetala,
+                    erhallet,
+                    erhalletBelopp,
+                    retur,
+                    tillbaka);
+                Console.WriteLine("-----------------------------");
+
+
+                double  vaxel = 0;
+
+                double femhundralapp = 0;
+                double etthundralapp = 0;
+                double tjugolapp = 0;
+                double femkrona = 0;
+                double enkrona = 0;
+
+                string femhundravaxel = "500-lappar";
+                string etthundravaxel = "100-lappar";
+                string tjugovaxel = "20-lappar";
+                string femkronavaxel = "5-kronor";
+                string enkronavaxel = "1-kronor";
+
+                vaxel = tillbaka;
+                femhundralapp = vaxel / femhundra;
+                vaxel = vaxel % femhundra;
+                etthundralapp = vaxel / etthundra;
+                vaxel = vaxel % etthundra;
+                tjugolapp = vaxel / tjugo;
+                vaxel = vaxel % tjugo;
+                femkrona = vaxel / fem;
+                vaxel = vaxel % fem;
+                enkrona = vaxel / en;
+                
+                Console.WriteLine(" {0}:{1:f0}\n {2}:{3:f0}\n {4}:{5:f0}\n {6}:{7:f0}\n {8}:{9:f0}",
+                    femhundravaxel, 
+                    femhundralapp,
+                    etthundravaxel,
+                    etthundralapp,
+                    tjugovaxel,
+                    tjugolapp,
+                    femkronavaxel,
+                    femkrona,
+                    enkronavaxel,
+                    enkrona);
             }
-        }        
     }
 }
---
+++ Laboration1TH/Laboration1TH/double working.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace Laboration1TH
+{
+    class Program
+    {
+        private const int femhundra = 500;
+        private const int etthundra = 100;
+        private const int tjugo = 20;
+        private const int fem = 5;
+        private const int en = 1;
+
+
+        static void Main(string[] args)
+        {
+
+            double totalKostnad = 0;
+            int erhalletBelopp = 0;
+            double attBetala = 0;
+            double oresavrundning = 0;
+            double tillbaka = 0;
+            string inmatatTal;
+            string angeTotalsumma = "Ange totalsumma";
+            string angeErhalletBelopp = "Ange erhållet belopp";
+
+            
+            // Ger felmeddelande ifall något annat än double skrivs in
+            while (true)
+            {
+                try
+                {
+                    Console.Write("{0,-20}: ",angeTotalsumma);
+                    inmatatTal = Console.ReadLine();
+                    totalKostnad = double.Parse(inmatatTal);
+                   //Ger felmeddelande och avslutar programmet ifall totalsumman är < 0,50
+                    if (totalKostnad <= 0.5)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Totalsumman är för liten. Köpet kunde itne genomföras!");
+                        Console.ResetColor();
+
+                        Environment.Exit(0);
+                    }
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("FEL! Erhållet belopp felaktigt.");
+                    Console.ResetColor();
+                }
+            }
+
+            //Ger felmeddelande ifall något annat än en int skrivs in
+            while (true)
+            {
+                try
+                {
+                    Console.Write("{0}: ",angeErhalletBelopp);
+                    inmatatTal = Console.ReadLine();
+                    erhalletBelopp = int.Parse(inmatatTal);                    
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("FEL! Erhållet belopp felaktigt.");
+                    Console.ResetColor();
+                }
+            }
+
+
+            if (erhalletBelopp < totalKostnad)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("FEL! Erhållet belopp är för litet. Köpet kunde inte genomföras. ");
+                Console.ResetColor();
+            }
+            else
+            {
+                //Öresavrunding 
+                attBetala = (uint)Math.Round(totalKostnad);
+                oresavrundning = attBetala - totalKostnad;
+
+                tillbaka = erhalletBelopp - attBetala;
+
+                string totalt = "Totalt";
+                string avrunding = "Öresavrundning";
+                string betala = "Att betala";
+                string erhallet = "Kontant";
+                string retur = "Tillbaka";
+
+                Console.WriteLine("\nKVITTO\n-------------------------------");
+
+                Console.WriteLine("{0,-17}:{1,12:c}\n{2,-17}:{3,12:c}\n{4,-17}:{5,12:c}\n{6,-17}:{7,12:c}\n{8,-17}:{9,12:c}",
+                    totalt,
+                    totalKostnad,
+                    avrunding,
+                    oresavrundning,
+                    betala,
+                    attBetala,
+                    erhallet,
+                    erhalletBelopp,
+                    retur,
+                    tillbaka);
+                Console.WriteLine("-------------------------------");
+
+
+
+                double vaxel = 0;
+                double femhundralapp = 0;
+                double etthundralapp = 0;
+                double tjugolapp = 0;
+                double femkrona = 0;
+                double enkrona = 0;
+
+                string femhundravaxel = "500-lappar";
+                string etthundravaxel = "100-lappar";
+                string tjugovaxel = "20-lappar";
+                string femkronavaxel = "5-kronor";
+                string enkronavaxel = "1-kronor";
+
+                vaxel = tillbaka;
+                femhundralapp = vaxel / femhundra;
+                vaxel = vaxel % femhundra;
+                etthundralapp = vaxel / etthundra;
+                vaxel = vaxel % etthundra;
+                tjugolapp = vaxel / tjugo;
+                vaxel = vaxel % tjugo;
+                femkrona = vaxel / fem;
+                vaxel = vaxel % fem;
+                enkrona = vaxel / en;
+
+                //Skriver ut antalet sedlar/mynt som kund skall få tillbaka samt rundar ner.
+                Console.WriteLine(" {0,-16}: {1:f0}\n {2,-16}: {3:f0}\n {4,-16}: {5:f0}\n {6,-16}: {7:f0}\n {8,-16}: {9:f0}",
+                    femhundravaxel,
+                    Math.Floor(femhundralapp),
+                    etthundravaxel,
+                    Math.Floor(etthundralapp),
+                    tjugovaxel,
+                    Math.Floor(tjugolapp),
+                    femkronavaxel,
+                    Math.Floor(femkrona),
+                    enkronavaxel,
+                    Math.Floor(enkrona));
+            }
+        }
+    }
+}
